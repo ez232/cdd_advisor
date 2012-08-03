4815136634d592286ad65da59708ffe32545c2d1
Rafael Camargo <rafael.camargo@gmail.com>
2012-08-03 20:42:29 +0100

    Rails project created.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CddAdvisor::Application.config.secret_token = '1b21af717a8cbec9e6c8dcad78c9b569e13acd161b939d01884ac1651e30b082163c6020c8f275219cd87f35ea1b73f632454b9e8fe8f43ca9e628ab1d6a5992'
