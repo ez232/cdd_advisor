c01a1763abedeab703ae2cdb49f3890d4e88188a
Rafael Camargo <rafael.camargo@gmail.com>
2012-08-23 08:29:44 +0100

    Iteration #22: Partials - New proposal nested forms moved to partials.

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -21,7 +21,8 @@ class Proposal < ActiveRecord::Base
   belongs_to :project
 
   accepts_nested_attributes_for :handle, :switch, :knob, :button, :screen,
-    :touch_screen, :label, :opening_device, :audio_device, :images
+    :touch_screen, :label, :opening_device, :audio_device, :images, 
+    allow_destroy: true
 
   # TODO: review attribute validation
   validates :name, presence: true, uniqueness: { scope: :project_id }
