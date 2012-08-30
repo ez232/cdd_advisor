5e1e2c79eaf9575cc2e33fa357305b1d1f7b751b
Rafael Camargo <rafael.camargo@gmail.com>
2012-08-30 16:07:02 +0100

    Iteration #26: Unlimited number of images per proposal implemented.

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -27,7 +27,6 @@ class ProposalsController < ApplicationController
   # GET /proposals/new.json
   def new
     @proposal = Proposal.new
-    3.times { @proposal.images.build }
 
     respond_to do |format|
       format.html # new.html.erb
